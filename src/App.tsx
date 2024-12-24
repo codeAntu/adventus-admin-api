045f1ab3dc5e8c77ea787b66ef15e5eb703196e1
Ananta <codeantu@gmail.com>
2024-12-24 18:09:32 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='bg-red-300 p-3'>Hello Vite + React!</div>
+    </>
+  )
+}
+
+export default App
